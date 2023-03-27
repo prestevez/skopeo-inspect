6e80e6425fde7888d39737f4de605711f11f14e2
Patricio R Estevez-Soto <patricio.estevez@ucl.ac.uk>
2023-03-27 14:25:35 -0600

    debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -lv
 
 skopeo inspect docker://alpine
-echo "$(skopeo inspect inspect docker://alpine)"
-tmp="$(skopeo inspect inspect docker://alpine)"
+echo "$(skopeo inspect docker://alpine)"
+tmp="$(skopeo inspect docker://alpine)"
 echo $tmp
 echo "inspection=$tmp" >> $GITHUB_OUTPUT
