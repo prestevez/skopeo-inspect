c94d1f173ee059b1245c561aa3bf9bb3b359ad51
Patricio R Estevez-Soto <patricio.estevez@ucl.ac.uk>
2023-03-27 15:50:33 -0600

    debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh -lve
 
 tmp="$(skopeo inspect $INPUT_TARGET_IMAGE)"
-cat $GITHUB_OUTPUT
-ls -lah /github/file_commands/
-cat /github/file_commands/set_output*
+
+## multi-line outputts
+
+EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
+echo "JSON_RESPONSE<<$EOF" >> $GITHUB_OUTPUT
 echo "inspection=$tmp" >> $GITHUB_OUTPUT
+echo "$EOF" >> $GITHUB_OUTPUT
 
 #echo "inspection='$(skopeo inspect $INPUT_TARGET_IMAGE)'" >> $GITHUB_OUTPUT
