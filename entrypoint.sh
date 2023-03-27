db7ffcbbbe820d4180943b5937149e35d186ff95
Patricio R Estevez-Soto <patricio.estevez@ucl.ac.uk>
2023-03-27 16:43:56 -0600

    debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,3 @@ EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
 echo "JSON_RESPONSE<<$EOF" >> $GITHUB_OUTPUT
 echo "inspection=$tmp" >> $GITHUB_OUTPUT
 echo "$EOF" >> $GITHUB_OUTPUT
-
-cat $GITHUB_OUTPUT
