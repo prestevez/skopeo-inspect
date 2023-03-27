30aec1744d31062276a0c463d4e4ea5a80587ecb
Patricio R Estevez-Soto <patricio.estevez@ucl.ac.uk>
2023-03-27 16:48:00 -0600

    debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,7 @@ tmp="$(skopeo inspect $INPUT_TARGET_IMAGE)"
 
 EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
 echo "JSON_RESPONSE<<$EOF" >> $GITHUB_OUTPUT
-echo "inspection=$tmp" >> $GITHUB_OUTPUT
+echo "inspection='$tmp'" >> $GITHUB_OUTPUT
 echo "$EOF" >> $GITHUB_OUTPUT
+
+cat $GITHUB_OUTPUT
