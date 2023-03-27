183c821377772c681eca252b01db8476b889c762
Patricio R Estevez-Soto <patricio.estevez@ucl.ac.uk>
2023-03-27 15:30:05 -0600

    debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -lve
 
-tmp="$(skopeo inspect $INPUT_TARGET_IMAGE)"
+#tmp="$(skopeo inspect $INPUT_TARGET_IMAGE)"
 cat $GITHUB_OUTPUT
-echo "inspection='$tmp'" >> $GITHUB_OUTPUT
+ls /github/file_commands/
+cat /github/file_commands/set_output*
+echo "inspection=$tmp" >> $GITHUB_OUTPUT
+
+#echo "inspection='$(skopeo inspect $INPUT_TARGET_IMAGE)'" >> $GITHUB_OUTPUT
