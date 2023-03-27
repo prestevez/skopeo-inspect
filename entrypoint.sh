3df84941605f9280b223ac1d278c47acf1c5719a
Patricio R Estevez-Soto <patricio.estevez@ucl.ac.uk>
2023-03-27 16:57:54 -0600

    debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ tmp="$(skopeo inspect $INPUT_TARGET_IMAGE)"
 ## multi-line outputts
 
 EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
-echo "inspection=<<$EOF" >> $GITHUB_OUTPUT
+echo "inspection<<$EOF" >> $GITHUB_OUTPUT
 echo "'$tmp'" >> $GITHUB_OUTPUT
 echo "$EOF" >> $GITHUB_OUTPUT
 
